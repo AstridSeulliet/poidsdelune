664a6c2d2a1f1859001c9f6227e76a8d1da632ea
Guillaume Boiret <guillaume@phantombuster.com>
2018-02-25 21:17:08 +0100

    clean server

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('public'))
+app.disable('x-pwerd-by')
+
+app.use(express.static(__dirname + '/public'))
+app.set('port', process.env.port || 3000)
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname+'/public/index.html');
+	res.sendFile(__dirname + '/views/index.html');
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(app.get('port'), () => {
+	console.log('Example app listening on port 3000!')
+})
