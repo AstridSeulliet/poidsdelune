a87adf8f19fd18ead96cd8e8bfdbc403518ba1f9
Guillaume Boiret <guillaume@phantombuster.com>
2018-03-05 00:08:19 +0100

    handle bad bodies in the API

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.disable('x-pwerd-by')
+app.disable('x-powered-by')
 app.use(express.static(__dirname + '/public'))
 app.set('port', process.env.port || 3000)
 
@@ -41,50 +41,56 @@ router.get('/', (req, res) => {
 
 router.route('/:body/:mass')
 	.get((req, res) => {
+		/*
+		Pesanteur sur la Terre : 9,81 N/kg
+		Pesanteur sur la Lune : 1,57 N/kg
+		Vous pesez (masse) 75 kg soit (75 x 9,81) 735,75 N sur Terre et (75 x 1,57) 117,75 N sur la Lune
+		Vous pesez donc environ (735,75 / 117,75) 6,25 fois moins sur la Lune
+		Une balance apportée sur la Lune vous donnerait donc un poids d'environ (75 / 6,25) 12 kg
+		Méthode plus directe : (Masse x Pesanteur Astre) / Pesanteur Terre
+		*/
 
-/*
-Pesanteur sur la Terre : 9,81 N/kg
-Pesanteur sur la Lune : 1,57 N/kg
-Vous pesez (masse) 75 kg soit (75 x 9,81) 735,75 N sur Terre et (75 x 1,57) 117,75 N sur la Lune
-Vous pesez donc environ (735,75 / 117,75) 6,25 fois moins sur la Lune
-Une balance apportée sur la Lune vous donnerait donc un poids d'environ (75 / 6,25) 12 kg
-Méthode plus directe : (Masse x Pesanteur Astre) / Pesanteur Terre
-*/
-
-		const GCList = { // Gravitational Constant
-			mercury: 1.57, // N/kg
-			venus: 1.57, // N/kg
-			earth: 9.81, // N/kg
-			mars: 1.57, // N/kg
-			jupiter: 1.57, // N/kg
-			saturn: 1.57, // N/kg
-			uranus: 1.57, // N/kg
-			neptum: 1.57, // N/kg
-			pluto: 1.57, // N/kg
+		const GFList = { // Gravitational field
+			mercury: 3.8, // N/kg
+			venus: 8.8, // N/kg
+			earth: 9.8, // N/kg
+			mars: 3.8, // N/kg
+			jupiter: 25.0, // N/kg
+			saturn: 10.4, // N/kg
+			uranus: 10.4, // N/kg
+			neptun: 13.8, // N/kg
 			moon: 1.57 // N/kg
 		}
 
 		const body = req.params.body
+		const bodyGF = GFList[body] // N/kg
 
-		const mass = parseInt(req.params.mass) // kg
-		const bodyGC = GCList[body] // N/kg
-		const earthGC = GCList["earth"] // N/kg
-		const pseudomass = (mass * bodyGC) / earthGC
-		const weight = mass * bodyGC
+		if (typeof bodyGF === "undefined") {
+			res.json({
+				error: "error"
+			})
+		} else {
+			const mass = parseInt(req.params.mass) // kg
+			const earthGF = GFList["earth"] // N/kg
+			const pseudomass = (mass * bodyGF) / earthGF // kg
+			const weight = mass * bodyGF // N
 
-		res.json({
-			result: {
-				source: {
-					body: "earth",
-					mass,
-					weight: mass * earthGC
-				},
-				target: {
-					body,
-					mass,
-					weight,
-					pseudomass
+			res.json({
+				result: {
+					source: {
+						body: "earth",
+						mass,
+						weight: mass * earthGF,
+						gravitational_field: earthGF
+					},
+					target: {
+						body,
+						mass,
+						weight,
+						gravitational_field: bodyGF,
+						pseudomass
+					}
 				}
-			}
-		})
+			})
+		}
 	})
